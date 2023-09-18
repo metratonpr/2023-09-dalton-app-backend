529318c48aff84ad48f601c62cf2142189678bb5
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-09-18 19:54:36 -0300

    Budget Details

--- app/Models/BudgetDetail.php
+++ app/Models/BudgetDetail.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class BudgetDetail extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'amount', 'price', 'discount', 'subtotal', 'budget_id',
+        'price_list_id'
+    ];
+
+    function budget()
+    {
+        return $this->belongsTo(Budget::class);
+    }
+
+    function priceList()
+    {
+        return $this->belongsTo(PriceList::class);
+    }
 }
