0e64748c5d499bcc316d31712b92910f4d1e30e4
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-09-18 19:45:56 -0300

    Price List

--- app/Models/PriceList.php
+++ app/Models/PriceList.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class PriceList extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['price', 'isAvailable', 'store_id', 'product_id'];
+
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function budgetDetails()
+    {
+        return $this->hasMany(BudgetDetail::class);
+    }
 }
