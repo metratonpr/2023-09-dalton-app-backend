f5c794a18c4559c21ab5b7b04976e403b27dd723
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-09-22 20:26:18 -0300

    Address Teste

--- tests/Feature/AddressTest.php
+++ tests/Feature/AddressTest.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class AddressControllerTest extends TestCase
+class AddressTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
 
