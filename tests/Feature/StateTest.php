f4ac27e157de1bbdecaccb5dfedfe515d86ff0f8
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-09-27 21:37:48 -0300

    Estado: teste criar

--- tests/Feature/StateTest.php
+++ tests/Feature/StateTest.php
@@ -10,7 +10,7 @@ use Tests\TestCase;
 
 class StateTest extends TestCase
 {
-    
+
     use RefreshDatabase, WithFaker;
 
     /**
@@ -18,7 +18,8 @@ class StateTest extends TestCase
      * Deve retornar 10 itens.
      * @return void
      */
-    public function test_salvar_pesquisar_estados_banco_dados(){
+    public function test_salvar_pesquisar_estados_banco_dados()
+    {
         //Preparar os dados ou parametros
         State::factory()->count(10)->create();
         //Processar
@@ -49,11 +50,81 @@ class StateTest extends TestCase
      * Criar um registro com sucesso
      * @return void
      */
-    public function test_criar_novo_estado_com_sucesso(){
+    public function test_criar_novo_estado_com_sucesso()
+    {
         //Criar um estado sem salvar
         $newData = StateFactory::new()->make()->getAttributes();
-        dd($newData);
+        //Processar
+        $response = $this->postJson('/api/states', $newData);
+
+        //Deu certo a solicitação = Status 201
+        $response->assertStatus(201);
+
+        // Verifique se a estrutura dos dados retornados está correta
+        $response->assertJsonStructure([
+            'id',
+            'name',
+            'country_id',
+            'created_at',
+            'updated_at',
+        ]);
+        //verificar o conteudo
+        $response->assertJson([
+            'name' => $newData['name'],
+            'country_id' => $newData['country_id'],
+        ]);
+    }
+
+    /**
+     * Tentar criar um registro e falhar
+     * @return void
+     */
+    public function test_criacao_estado_com_falha()
+    {
+        $newData = [
+            'name' => '', 
+            'country_id' => 99999, 
+        ];
+
+        // Faça uma requisição POST para a rota de criação
+        $response = $this->postJson('/api/states', $newData);
+
+        // Verifique se a resposta tem status 422
+        $response->assertStatus(422);
+
+        // Verifique se há erros de validação 
+        $response->assertJsonValidationErrors(['name', 'country_id']);
+    }
+
+    /**
+     * Tentar salvar com nome duplicado e falhar
+     * @return void
+     */
+    public function test_tentar_salvar_estado_com_mesmo_nome_falhar(){
+
+        //Criar um teste
+        $data  = State::factory()->create();
+        $newData = [
+            'name'=>$data->name,
+            'country_id'=>$data->country_id
+        ];
+
+        // Faça uma requisição POST para a rota de criação
+        $response = $this->postJson('/api/states', $newData);
+
+        // Verifique se a resposta tem status 422
+        $response->assertStatus(422);
+
+        // Verifique se há erros de validação 
+        $response->assertJsonValidationErrors(['name']);
 
     }
 
+    /**
+     * Criar um estado e pesquisar pelo seu id
+     * @return void
+     */
+     public function test_criar_estado_pesquisar_pelo_id(){
+
+     }
 }
